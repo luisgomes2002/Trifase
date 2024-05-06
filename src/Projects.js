5aa740ecfffdf182b64e0f4b0cd3fee690642f0c
Luis <luisgmgomes@gmail.com>
2024-05-05 21:10:32 -0300

    chore: update

--- src/Projects.js
+++ src/Projects.js
@@ -1,3 +1,18 @@
+//Home video
+const btn = document.querySelector(".switch-btn");
+const video = document.querySelector(".video-container");
+
+btn.addEventListener("click", () => {
+  if (!btn.classList.contains("slide")) {
+    btn.classList.add("slide");
+    video.pause();
+  } else {
+    btn.classList.remove("slide");
+    video.play();
+  }
+});
+
+//Show images
 document.addEventListener("DOMContentLoaded", () => {
   const grid = document.querySelector(".showProjects");
   const imagesWithText = [
